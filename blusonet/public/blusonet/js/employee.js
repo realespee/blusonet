1a4a9953e09b93e39e54d9971b4f92deca358399
realespee <wanyamasp@gmail.com>
2022-05-25 02:31:09 +0300

    include employee doctype js override

--- blusonet/public/blusonet/js/employee.js
+++ blusonet/public/blusonet/js/employee.js
@@ -1,5 +1,13 @@
 frappe.ui.form.on("Employee", {
     on_load: function (frm) {
-        console.log(frm)
+        if (frm.doc.company == "BluSonet") {
+            frm.pan_number.label = "TIN"
+        }
+        else if(frm.doc.company == "Blu-UnitedStates") {
+            frm.pan_number.label = "TIN Number"
+        }
+        else{
+            frm.pan_number.label = "PAN Number"
+        }
     }
 });
