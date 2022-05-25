bcea40efc40fa948afd87aec03e80db0275011a3
realespee <wanyamasp@gmail.com>
2022-05-25 09:24:56 +0300

    chnage field label from PAN Number to TIN

--- blusonet/hooks.py
+++ blusonet/hooks.py
@@ -37,7 +37,7 @@ fixtures = [
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Employee" : "public/blusonet/js/employee.js"}
+doctype_js = {"Employee" : "public/js/employee.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
