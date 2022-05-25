cc2419c5f76b95902811f0f4287da2a18853e9f4
realespee <wanyamasp@gmail.com>
2022-05-25 10:50:28 +0300

    change fieldname on refresh

--- blusonet/public/js/employee.js
+++ blusonet/public/js/employee.js
@@ -9,5 +9,15 @@ frappe.ui.form.on("Employee", {
             frm.set_df_property("pan_number", "label", "TIN Number")
             frm.refresh()
         }
+    },
+
+    refresh(frm) {
+        
+        let company = frm.doc.company
+
+        if ((company == "BluSonet") || (company == "Blu-UnitedStates")) {
+            // change fieldname from PAN Number to TIN
+            frm.set_df_property("pan_number", "label", "TIN Number")
+        }
     }
 });
